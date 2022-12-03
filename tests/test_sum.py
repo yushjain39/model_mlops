80c9562aa2d4701d1b1f818b9a7b882fa0a1e6cd
yushjain39 <yushjain39@gmail.com>
2022-12-03 15:56:05 +0530

    automate code review

--- tests/test_sum.py
+++ tests/test_sum.py
@@ -1,10 +1,12 @@
-from apps.sum import add
+from apps.sum import add, sub
 
 def test_add():
 
     assert add(9,6) == 15
     print("-----------------------test passed successfully-----------------------")
-
+def test_sub():
+    
+    assert sub(9,6) == 3
 
 if __name__ == "__main__":
 
